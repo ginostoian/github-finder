1bd9a9d0a4ac8016ce6e14e5ce468da88512bbbc
Gino Stoian <ginostoian@gmail.com>
2022-05-01 22:21:07 +0100

    switch state in context for useReducer

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,15 +1,20 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
+
+import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext()
 
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+// const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 
 export const GithubProvider = ({ children }) => {
     //Instantiate state to hold the fetched data
-    const [users, setUsers] = useState([])
-    //Instantiate state to show a loader while fetching data
-    const [isLoading, setIsLoading] = useState(true)
+    const initialState = {
+        users: [],
+        isLoading: true
+    }
+
+    const [state, dispatch] = useReducer(githubReducer, initialState)
 
     const fetchUsers = async () => {
         const res = await fetch(`${GITHUB_URL}/users`, {
@@ -19,13 +24,16 @@ export const GithubProvider = ({ children }) => {
         })
 
         const data = await res.json()
-        setUsers(data)
-        setIsLoading(false)
+
+        dispatch({
+            type: 'GET_USERS',
+            payload: data
+        })
     }
 
     return <GithubContext.Provider value={{
-        users,
-        isLoading,
+        users: state.users,
+        isLoading: state.isLoading,
         fetchUsers
     }}>
         {children}
