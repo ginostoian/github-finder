10724af0e32c6bf8e03ccff4d5c6aad2397a7a05
Gino Stoian <ginostoian@gmail.com>
2022-05-01 12:04:57 +0100

    add text to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,28 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React from 'react'
 
 function About() {
     return (
-        <div>
-            About
-        </div>
+        <>
+            <h1 className='text-6xl mb-4'>Github Finder</h1>
+            <p className='mb-4 text-lg font-light text-white'>
+                A React app to search GitHub profiles and see profile details. This
+                project has been created during my journey to learn React. See this and more projects on
+                <strong className='text-xl'>
+                    <a target='_blank' href='https://ginostoian.com' rel="noopener"> my portfolio</a>
+                </strong>
+                .
+            </p>
+            <p className='text-lg text-gray-400'>
+                Version <span className='text-white'>1.0.0</span>
+            </p>
+            <p className='text-lg text-gray-400'>
+                Created By: {' '}
+                <a className='text-white' href='https://ginostoian.com' target='_blank' rel='noopener'>
+                    Gino Stoian
+                </a>
+            </p>
+        </>
     )
 }
 
