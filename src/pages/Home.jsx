0c99c134180edf9722edecf5b406b48a4ca8fbfc
Gino Stoian <ginostoian@gmail.com>
2022-05-05 23:58:08 +0100

    add some info on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,11 @@ import UserResults from '../components/users/UserResults'
 function Home() {
     return (
         <>
+            <div class="mockup-code mb-32 max-w-lg mx-auto md:mx-0">
+                <pre data-prefix="$"><code>search for github users</code></pre>
+                <pre data-prefix=">" class="text-warning"><code>fetching data...</code></pre>
+                <pre data-prefix=">" class="text-success"><code>Happy stalking!</code></pre>
+            </div>
             <UserSearch />
             <UserResults />
         </>
